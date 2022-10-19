14f545393e24f5411c3fe434c64a265e7b2ce468
Nick A Lanese <NLanese21@gmail.com>
2022-10-19 11:47:29 -0400

    We got it all working just like previously

--- src/utils/pusherClient.js
+++ src/utils/pusherClient.js
@@ -1,7 +1,8 @@
-import Pusher from "pusher-js";
+// import Pusher from "pusher-js";
 
-var pusherClient = new Pusher("58efed017348be8ec435", {
-  cluster: "us2",
-});
+var pusherClient = "wingo"
+// new Pusher("58efed017348be8ec435", {
+//   cluster: "us2",
+// });
 
 export default pusherClient
