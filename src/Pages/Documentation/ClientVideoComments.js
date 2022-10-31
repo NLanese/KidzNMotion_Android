b4d83eefd3a4e6bdef3c492113da35a38afc61a5
Nick A Lanese <NLanese21@gmail.com>
2022-10-30 23:33:32 -0400

    Update ClientVideoComments.js

--- src/Pages/Documentation/ClientVideoComments.js
+++ src/Pages/Documentation/ClientVideoComments.js
@@ -1,24 +1,26 @@
-// Reaact
+// React
 import { View, Text, TextInput, ScrollView, TouchableOpacity, Dimensions } from "react-native";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigation } from "@react-navigation/native";
 import Modal from "react-native-modal";
-
 
 // Nuton
 import { Button, Header } from "../../../NutonComponents";
 
 // Recoil
 import { useRecoilValue, useRecoilState } from "recoil";
-import {sizeState, clientListState, userState, colorState, fontState, avatarState, videoDataState } from '../../../Recoil/atoms';
+import {sizeState, clientListState, userState, colorState, fontState, avatarState, videoDataState, tokenState } from '../../../Recoil/atoms';
 
 // GraphQL
-import { CREATE_COMMENT } from "../../../GraphQL/operations";
+import { CREATE_COMMENT, GET_USER } from "../../../GraphQL/operations";
 import { useMutation } from "@apollo/client";
+import client from "../../utils/apolloClient";
 
-// Ostrich vbn5
+// Ostrich 
 import Gradient from "../../../OstrichComponents/Gradient";
-import { white } from "material-ui/styles/colors";
+
+// Hooks
+import convertReactCalandarDateString from "../../Hooks/date_and_time/convertReactCalandarDateString"
 
 // Dimensions
 let maxWidth = Dimensions.get('window').width
@@ -39,7 +41,6 @@ export default function ClientVideoComments(props) {
         const plan = client1.plan
         const client = client1.user
 
-
         const COLORS = useRecoilValue(colorState)
         const FONTS = useRecoilValue(fontState)
         const SIZES = useRecoilValue(sizeState)
@@ -53,17 +54,59 @@ export default function ClientVideoComments(props) {
         // Determines which video and which comments will be shown in the comment modal
         const [selectedVid, setSelectedVid] = useState(false)
 
+        const [selectedComments, setSelectedComments] = useState([])
+
         // Determines whether the comment modal is open or not
         const [modalOpen, setModalOpen] = useState(false)
 
         // Tracks the current type
         const [textEntered, setTextEntered] = useState('')
 
+        // Tracks all ids that have comments
+        const [vidIds, setVidIds] = useState([])
+
+        // Object with a key/value pair for each video/commentList
+        const [commentIds, setCommentIds] = useState({
+            test: "empty"
+        })
+
     //////////////////
     // Recoil State //
     //////////////////
 
     const [videos, setVideos] = useRecoilState(videoDataState)
+
+    const [user, setUser] = useRecoilState(userState)
+
+    const token = useRecoilValue(tokenState)
+
+    ////////////////
+    // UseEffects //
+    ////////////////
+
+    // Populates CommentIds object upon rendering
+    useEffect(() => {
+        // Local variable to track new additions
+        let newComments = commentIds
+
+        // Goes through ever Comment
+        plan.comments.forEach( (comment, i) => {
+
+            // If a video id alredy has comments, add this to it
+            if (newComments[(comment.videoId)]){
+                newComments = {...newComments, [comment.videoId]: [...newComments[comment.videoId], comment]}
+            }
+
+            // If a video id does not yet have a comment
+            else{
+                newComments = {...{...newComments}}
+                newComments = {...newComments, [comment.videoId]: [{...comment}]}
+            }
+        })
+
+        // Sets the overall object
+        setCommentIds( commentIds => ({...{...newComments}}))
+    }, [client1])
 
 
     ///////////////
@@ -107,10 +150,17 @@ export default function ClientVideoComments(props) {
     // Renders all Video Tabs with Comments 
     function renderAllVideoComments(){
         return videos.map( (video, index) => {
+            let count = 0
+            let theseComments = []
+            let tag = `${video.id}`
+            if (commentIds[tag]){
+                theseComments = commentIds[tag]
+                count = commentIds[tag].length
+            }
             return (
                 <TouchableOpacity 
                 key={index}  
-                onPress={() => handleHeaderClick(video)}
+                onPress={() => handleHeaderClick(video, theseComments)}
                 style={{marginTop: 12, marginRight: 4, marginLeft: 4, borderColor: 'white', borderWidth: 0.5, paddingTop: 10, paddingBottom: 10, paddingRight: 4, paddingLeft: 4, borderRadius: 6, flexDirection: 'row'}}>
                     <View style={{flex: 7}}>
                         <Text style={{...FONTS.SubTitle, color: 'white', fontSize: 22}}>
@@ -119,7 +169,7 @@ export default function ClientVideoComments(props) {
                     </View>
                     <View style={{flex: 3}} >
                         <Text style={{...FONTS.SubTitle, color: 'white', fontSize: 18}}>
-                            X Comments
+                            {count} Comments
                         </Text>
                     </View>
                 </TouchableOpacity>
@@ -162,16 +212,33 @@ export default function ClientVideoComments(props) {
     function renderCommentContent(){
         return(
             <View>
-                <Text style={{...FONTS.SubTitle, textAlign: 'center', marginBottom: 4}}>
+                <Text style={{...FONTS.SubTitle, fontSize: 20, textAlign: 'center', marginBottom: 4}}>
                     Comments for {client1.user.firstName} {client1.user.lastName}
                 </Text>
-
                 <Text style={{...FONTS.SubTitle, textAlign: 'center', marginBottom: 10}}>
                     {selectedVid.title}
                 </Text>
+                <View style={{marginTop: 10, marginBottom: 10}}>
+                    {renderCommentsForVideo()}
+                </View>
                 {renderAddCommentSpace()}
             </View>
         )
+    }
+
+    function renderCommentsForVideo(){
+        return selectedComments[0].map( (com, i) => {
+            return(
+                <View key={i} style={{borderWidth: 1, borderColor: 'black', padding: 10}}>
+                    <Text style={{textAlign: 'center', marginTop: -6, fontSize: 12}}>
+                        {convertReactCalandarDateString(com.createdAt.split("T")[0])}
+                    </Text>
+                    <Text style={{textAlign: 'center', fontSize: 16, marginTop: 5}}>
+                        {`${com.content}`}
+                    </Text>
+                </View>
+            )
+        })
     }
 
     // Renders the Text Input as well as the send button
@@ -200,14 +267,19 @@ export default function ClientVideoComments(props) {
 ///////////////////////
 
     // Runs on Pressing a Comment Header
-    function handleHeaderClick(video){
+    function handleHeaderClick(video, comments){
         setSelectedVid(video)
+        setSelectedComments(selectedComments => ([comments]))
         setModalOpen(true)
     }
 
     // Runs the Save Comment Process
     function handleSaveComment(video){
         createCommentMutation(video)
+        .then(() => {
+            setTextEntered("")
+            setModalOpen(false)
+        })
     }
 
     // Runs the Create Comment Mutation
@@ -222,6 +294,20 @@ export default function ClientVideoComments(props) {
         .catch(err => console.log(err))
         .then((resolved) => {
             console.log(resolved)
+        })
+    }
+
+    // Gets Refreshed User Object and Updates the User Atom
+    async function getAndSetUser(){
+        await client.query({
+            query: GET_USER,
+            fetchPolicy: 'network-only'  
+        })
+        .then(async (resolved) => {
+            // setUser(resolved.data.getUser)
+        })
+        .catch((error) => {
+            console.log(error, "============\n591\n===========")
         })
     }
 
