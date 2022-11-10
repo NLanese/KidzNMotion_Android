6ebebcae03763f1caf65b09f68c086b0326d653f
Nick A Lanese <NLanese21@gmail.com>
2022-11-10 14:20:11 -0500

    Retuned Loading Component to host medal gifs

--- src/Global/LoadingComponent.js
+++ src/Global/LoadingComponent.js
@@ -6,65 +6,70 @@ import FastImage from "react-native-fast-image";
 import Modal from "react-native-modal";
 
 import gif1 from "./XOsX.gif"
-import gif2 from "./yy3.gif"
-
+import bronze from "./medalGifs/bronze-coin.gif"
+import silver from "./medalGifs/Silver-Coin.gif"
+import gold from "./medalGifs/GoldCoin-Gif.gif"
 
 let maxWidth = Dimensions.get('window').width
 let maxHeight = Dimensions.get('window').height
 
 
-export default function LoadingComponent({loading, label}) {
+export default function LoadingComponent({loading, setLoading=null, label, source=1, dismiss=false}) {
 
     if (!loading){
         return null
     }
-    let source
-    switch(Math.ceil(Math.random()*2)){
-        // case 1:
-        //     source = gif2
-        //     break
+    switch(source){
+        case 1:
+            source = gif1
+            break
+        case 2:
+            source = bronze
+            break
+        case 3:
+            source = silver
+            break
+        case 4: 
+            source = gold
+            break
         default:
             source = gif1
+            
+    }
+
+    function handleDismiss(){
+        if (dismiss){
+            setLoading(false)
+            loading = false
+        }
+        else{
+            return null
+        }
     }
     
     return(
          <Modal
                 isVisible={loading}
-                onBackdropPress={null}
+                onBackdropPress={() => handleDismiss()}
                 hideModalContentWhileAnimating={false}
                 backdropTransitionOutTiming={0}
-                style={{ margin: 0 }}
+                style={{ margin: 50 }}
                 animationIn="zoomIn"
                 animationOut="zoomOut"
             >
-            <View style={{postion: "absolute", top: 0, start: 0, left: 0, zIndex: 999, height: maxHeight, width: "100%", justifyContent: "center"}}>
+            <View style={{postion: "absolute", top: 0, start: 0, left: '25%', zIndex: 999, height: maxHeight * 0.9, width: "50%", justifyContent: "center"}}>
                     
                     <FastImage
                         style={{ width: 500, height: maxHeight*.5, alignSelf: "center", postion: "absolute" }}
                         source={source}
                         resizeMode={FastImage.resizeMode.contain}
                     />
-                    <Text style={{alignSelf:"center"}}>{label}</Text>
+                    <Text style={{alignSelf:"center", fontFamily: "Gilroy-Bold", color: 'white', fontSize: 24}}>
+                        {label}
+                    </Text>
                 </View>
         </Modal>
     )
-    
-    
-    
-    //absolute position is not working
-    //trying to get this to work as a modal
-    return (
-                
-                <View style={{postion: "absolute", top: 0, start: 0, left: 0, zIndex: 999, height: maxHeight, width: "100%", justifyContent: "center"}}>
-                    
-                    <FastImage
-                        style={{ width: 500, height: maxHeight*.5, alignSelf: "center", postion: "absolute" }}
-                        source={source}
-                        resizeMode={FastImage.resizeMode.contain}
-                    />
-                    <Text style={{alignSelf:"center"}}>{label}</Text>
-                </View>
-            )
     
 }
 
