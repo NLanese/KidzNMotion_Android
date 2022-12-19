e6dad83daa981541d6c2c3abb9b763b986f1b228
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2022-12-19 12:00:52 -0500

    Update ProfileEdit.js

--- src/Pages/Settings/ProfileEdit.js
+++ src/Pages/Settings/ProfileEdit.js
@@ -590,7 +590,7 @@ export default function ProfileEdit() {
                         justifyContent: "center", alignItems: "center",
                         marginHorizontal: 7.5,   
                     }}
-                    onPress={() => { console.log("moo()") }}
+                    onPress={() => { setDeleteModal(false) }}
                 >
                     <Text style={{ color: COLORS.black, ...FONTS.ModalButton, textAlign: 'center'}}>
                         Cancel
